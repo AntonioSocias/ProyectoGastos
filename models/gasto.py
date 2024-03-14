b9c16134a11646f979fc80a8a2dc605071384c7d
AntonioSocias <146021038+AntonioSocias@users.noreply.github.com>
2024-03-14 16:28:09 +0000

    fa

--- models/gasto.py
+++ models/gasto.py
@@ -6,8 +6,14 @@ class Gasto(db.Model):
     titulo = db.Column(db.String(100), nullable=False)
     cantidad = db.Column(db.Numeric(precision=10, scale=2), nullable=False)
     fecha = db.Column(db.Date, nullable=False)
-    proyecto = db.Column(db.Integer, db.ForeignKey('proyecto.id'), nullable=False)
-    pagador = db.Column(db.Integer, db.ForeignKey('usuario.id'), nullable=False)
+    proyecto_id = db.Column(db.Integer, db.ForeignKey('proyectos.id'), nullable=False)
+    pagador_id = db.Column(db.Integer, db.ForeignKey('usuarios.id'), nullable=False)
+    
+    # Relación con la tabla proyectos
+    proyecto = db.relationship('Proyecto', backref='gastos')
+    
+    # Relación con la tabla usuarios
+    pagador = db.relationship('Usuario', backref='gastos')
 
     @classmethod
     def get_by_id(cls, id):
