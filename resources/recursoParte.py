89a7fb79ba7e24be3116194768946b919674ebc8
AntonioSocias <146021038+AntonioSocias@users.noreply.github.com>
2024-04-11 11:15:55 +0000

    modificacion

--- resources/recursoParte.py
+++ resources/recursoParte.py
@@ -1,7 +1,7 @@
 from flask import request
 from flask_restful import Resource
 from http import HTTPStatus
-from models.parte import Parte
+from models.modelosGeneral import Parte
 from extensiones import db
 
 class ParteListResource(Resource):
@@ -22,7 +22,8 @@ class ParteListResource(Resource):
             descripcion = datos.get('descripcion'),
             foto = datos.get('foto')
         )
-        parte.guardar()
+        db.session.add(parte)
+        db.session.commit()
         return parte.data, HTTPStatus.CREATED
 
 class ParteResource(Resource):
@@ -46,7 +47,8 @@ class ParteResource(Resource):
         parte.fecha = datos.get('fecha')
         parte.descripcion = datos.get('descripcion')
         parte.foto = datos.get('foto')
-        parte.guardar()
+        db.session.add(parte)
+        db.session.commit()
         
         # No es necesario devolver el parte actualizado, ya se puede acceder a él a través de su ID
         return parte.data, HTTPStatus.OK
